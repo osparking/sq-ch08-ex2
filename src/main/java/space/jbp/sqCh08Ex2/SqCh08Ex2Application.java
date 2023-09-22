86e0c5289c0f317ce0e10c8327aa3025323d0d31
osparking <jbpark03@gmail.com>
2023-09-22 09:51:22 +0900

    initial w/ 2 dependencies

---
+++ src/main/java/space/jbp/sqCh08Ex2/SqCh08Ex2Application.java
@@ -0,0 +1,13 @@
+package space.jbp.sqCh08Ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh08Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh08Ex2Application.class, args);
+	}
+
+}
