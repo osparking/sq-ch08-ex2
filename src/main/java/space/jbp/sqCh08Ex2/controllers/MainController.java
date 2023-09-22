63a11866a0e9eec73f07dbca5db42dc33ada50a8
osparking <jbpark03@gmail.com>
2023-09-22 14:13:21 +0900

    action method accesses request param

--- src/main/java/space/jbp/sqCh08Ex2/controllers/MainController.java
+++ src/main/java/space/jbp/sqCh08Ex2/controllers/MainController.java
@@ -3,13 +3,14 @@ package space.jbp.sqCh08Ex2.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 	@GetMapping("/home")
-	public String home(Model page) {
+	public String home(Model page, @RequestParam String color) {
 		page.addAttribute("username", "jbpark03");
-		page.addAttribute("color", "deeppink");
+		page.addAttribute("color", color);
 		return "home.html";
 	}
 }
